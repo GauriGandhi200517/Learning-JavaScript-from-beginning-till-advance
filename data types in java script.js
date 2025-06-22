bd094b0a2d599cc844fdd344ab401cfb1633c780
Gauri gandhi <gauri.gandhi4827@gmail.com>
2025-06-23 01:58:34 +0530

    Create data types in java

---
+++ data types in java script.js
@@ -0,0 +1,29 @@
+//  Data types in JavaScript
+// JavaScript has dynamic typing, meaning variables can hold any type of data
+/** string**/
+let username ="Gauri";
+console.log(username); // Output: Gauri
+/** number **/
+let age = 20;
+console.log(age); // Output: 25
+/** boolean true/false--> yes/no **/
+let isproductincart= true;
+console.log(isproductincart); // Output: true
+let isproductincart2 = false;
+console.log(isproductincart); // Output: false
+/** undefined **/
+let username2;
+console.log(username2); // Output: undefined
+/** null **/
+let user = null;
+console.log(user); // Output: null
+/** object **/
+let userDetails = {
+    name: "Gauri",
+    age: 20,
+    isStudent: true
+};
+console.log(userDetails); // Output: { name: 'Gauri', age: 20, isStudent: true }
+/** array **/
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits); // Output: [ 'apple', 'banana', 'cherry'] 
