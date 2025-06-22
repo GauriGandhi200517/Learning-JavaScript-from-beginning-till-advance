88c69391c99f94a4e70f22df1a6081c1ba722723
Gauri gandhi <gauri.gandhi4827@gmail.com>
2025-06-22 14:31:01 +0530

    create varible naming convention

---
+++ variable naming convention.js
@@ -0,0 +1,10 @@
+let username;
+let userage;
+// Variable naming conventions in JavaScript
+// 1. Variable names must start with a letter, dollar sign ($), or underscore (_)
+let $variableName;
+let _variableName;
+// 2. Variable names can contain letters, numbers, dollar signs, and underscores
+// dollar sign and underscore are allowed in variable names
+let $="hello";
+console.log($); 
