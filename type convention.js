374ae9e685de27f6a92a08038649744e6ab92934
Gauri gandhi <gauri.gandhi4827@gmail.com>
2025-06-23 21:48:09 +0530

    create type conventions

---
+++ type convention.js
@@ -0,0 +1,23 @@
+/* Type Conversions 
+*String 📝
+*Number 🔢
+*Boolean ✅❌
+*/
+const a="3";
+const b="10";
+const c= Number(a)
+const d= Number(b)
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+const e= "Gauri";
+const f= "20";
+console.log(boolean(f))
+//example 
+console.log(Boolean("Gauri")); // Outputs: true
+console.log(Boolean("")); // Outputs: false
+console.log(Boolean(0)); // Outputs: false
+console.log(Boolean(100)); // Outputs: true
+console.log(Boolean(-1)); // Outputs: true
